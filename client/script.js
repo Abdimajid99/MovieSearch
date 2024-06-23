4dc7a419d0e94a4a1e99b842b7beb4237aa493e4
Abdimajid Shire <Abdulmjeedshire3@gmail.com>
2024-06-23 22:14:38 +0300

    feat: feat: search for a movie and display the results

--- client/script.js
+++ client/script.js
@@ -1,26 +1,50 @@
 "use strict";
 
-const url = new URL(`http://localhost:3001`);
-
-url.searchParams.set("title", "The");
-url.toString();
-console.log(url);
-console.log("the actual url", url.toString());
-
-fetch(url)
-  .then((res) => res.json())
-  .then((res) => console.log(res));
-
-//
-
 //! MAIN TO-DOS.
 
-//TODO : put this project in github.
+//done - TODO : put this project in github.
 //TODO: create the frontend
+//!       -- do the modal functionality
+//!       -- put a heading that says ' no movies with this title have been found' when there are no movies to be shown.
+//!       -- watch fireships course vids again to make sure everything is good.
 //TODO: host it on render
 
 //TODO: create terminal quiz game
 //TODO: make a resume
 
 //TODO: apply to the source graph job
+
 //
+
+const formEle = document.querySelector("form");
+const searchInput = document.querySelector("form input");
+const moviesContainer = document.querySelector(".movies-container");
+
+const displayMovies = function (movies) {
+  moviesContainer.innerHTML = "";
+
+  movies.forEach(({ _id, title, poster, year, imdb: { rating } }) => {
+    const html = `
+    <div class="movie" title="${title}" id=${_id}>
+        <div class="image-wrapper"> <img src="${poster}" alt=" poster for the movie: ${title}"></div>
+        <div class="info">
+            <p class="title">${title} (${year})</p>
+            <span>${rating}</span>
+        </div>
+    </div>
+        `;
+
+    moviesContainer.insertAdjacentHTML("beforeend", html);
+  });
+};
+
+formEle.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const searchedTitle = searchInput.value.trim();
+
+  const res = await fetch(`http://localhost:3001/?title=${searchedTitle}`);
+  const movies = await res.json();
+  // console.log(movies);
+
+  displayMovies(movies);
+});
